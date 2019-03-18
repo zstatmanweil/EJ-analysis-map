b5935be97ce50ea8ef2fd08407f13198c49399a1
zstatmanweil <zstatmanweil@gmail.com>
2019-03-18 12:04:44 -0400

    Made minor tweaks

--- web-display/app.py
+++ web-display/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 def index():
     mapbox_access_token = 'pk.my_mapbox_access_token'
-    return render_template("main.html", { 'mapbox_access_token': mapbox_access_token })
+    return render_template("main.html", mapbox_access_token=mapbox_access_token)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(port=5000, debug=True)
