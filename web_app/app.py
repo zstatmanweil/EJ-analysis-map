ba442d270c8cc1ae992df6ac3d799d7c624c3be5
zstatmanweil <zstatmanweil@gmail.com>
2019-04-02 15:08:44 -0400

    updated templates, rearranged site to include a navbar

--- web_app/app.py
+++ web_app/app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("home.html")
+
+
+@app.route('/map')
+def map():
     mapbox_access_token = 'pk.my_mapbox_access_token'
-    return render_template("main.html", mapbox_access_token=mapbox_access_token)
+    return render_template("map.html", mapbox_access_token=mapbox_access_token)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
