39b551b6fa27b2c4761ff16abe2ab044d9d59034
zstatmanweil <zstatmanweil@gmail.com>
2019-04-19 18:31:20 -0400

    Updated style of home page

--- web_app/app.py
+++ web_app/app.py
@@ -12,5 +12,9 @@ def map():
     mapbox_access_token = 'pk.my_mapbox_access_token'
     return render_template("map.html", mapbox_access_token=mapbox_access_token)
 
+@app.route('/results')
+def results():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
